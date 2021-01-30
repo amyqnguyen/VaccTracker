02e046f8babab40e7b8aea6b10069a18e7e5892e
Amy <amyqnguyen@outlook.com>
2021-01-29 23:39:45 -0800

    Added Signin page and linked pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import './App.css';
-import LoginPage from './pages/loginpage';
+import SignUpPage from './pages/signuppage';
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom';
+import LoginPage from './pages/loginpage';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>Covid-19 Vaccine Finder</h1>
-      </header>
-      <LoginPage />
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Covid-19 Vaccine Finder</h1>
+        </header>
+        <a href="/signuppage">Click here to get started! (placeholder)</a>
+        <div>
+          <Switch>
+            <Route path="/signuppage" component={SignUpPage} />
+            <Route path="/loginpage" component={LoginPage} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
