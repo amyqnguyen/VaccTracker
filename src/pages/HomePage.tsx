a80592683e03e749d144c5ea19888d5b4804a8f3
Jeffrey Kwok <47469240+jkwok2@users.noreply.github.com>
2021-01-30 15:24:06 -0800

    changed something

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -81,7 +82,7 @@ export default function Album() {
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <CameraIcon className={classes.icon} />
+          <CheckCircleIcon className={classes.icon} />
           <Typography variant="h6" color="inherit" noWrap>
             COVID-19 Vaccine Finder
           </Typography>
