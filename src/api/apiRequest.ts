94b686522027e75ec186130c258e4fe50e72453c
Patrick Chan <chan.cy.patrick@gmail.com>
2021-01-30 17:19:26 -0800

    changed something (#4)

---
+++ src/api/apiRequest.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { API_ENDPOINT } from './constants';
+import { User } from './types/User';
+
+export async function apiAddUser(body: { firstName: string, lastName: string, role: 'patient'|'clinic' }) {
+  axios.put(`${API_ENDPOINT}/v1/user`, body);
+}
+
+export async function apiGetUserData(id: string) {
+  const response = await axios.get<User>(`${API_ENDPOINT}/v1/user/${id}`);
+  return response.data;
+}
+
+export async function apiUpdatePatientData(id: string, body: { priority: string, postalCode: string }) {
+  axios.post(`${API_ENDPOINT}/v1/user/${id}`, body);
+}
+
+export async function apiGetWaitlist() {
+  const response = await axios.get<User[]>(`${API_ENDPOINT}/v1/user/list`);
+  return response.data;
+}
+
+export async function apiGetVaccines(id: string) {
+  const response = await axios.get(`${API_ENDPOINT}/v1/clinic/${id}/vaccine`);
+  return response.data;
+}
+
+export async function apiAddVaccines(id: string, body: { name: string, inventory: number }) {
+  axios.post(`${API_ENDPOINT}/v1/clinic/${id}/vaccine`, body);
+}
