b3b0f45b225b4ec895fb9beb6ec3779c480c6e1d
patchan <11303629+patchan@users.noreply.github.com>
2021-01-31 00:31:53 -0800

    fix endpoints

--- src/api/apiRequest.ts
+++ src/api/apiRequest.ts
@@ -1,35 +1,34 @@
 import axios from 'axios';
-import { API_ENDPOINT } from './constants';
 import { User } from './types/User';
 
 export async function apiAddUser(body: { email: string, name: string, role: 'patient'|'clinic' }) {
-  axios.post(`${API_ENDPOINT}/v1/user`, body);
+  axios.post('/v1/user', body);
 }
 
 export async function apiGetUserData(id: string) {
-  const response = await axios.get<User>(`${API_ENDPOINT}/v1/user/${id}`);
+  const response = await axios.get<User>(`/v1/user/${id}`);
   return response.data;
 }
 
 export async function apiUpdatePatientData(id: string, body: { priority: string, postalCode: string }) {
-  axios.post(`${API_ENDPOINT}/v1/user/${id}`, body);
+  axios.post(`/v1/user/${id}`, body);
 }
 
 export async function apiGetWaitlist() {
-  const response = await axios.get<User[]>(`${API_ENDPOINT}/v1/user/list`);
+  const response = await axios.get<User[]>(`/v1/user/list`);
   return response.data;
 }
 
 export async function apiGetVaccines(id: string) {
-  const response = await axios.get(`${API_ENDPOINT}/v1/clinic/${id}/vaccine`);
+  const response = await axios.get(`/v1/clinic/${id}/vaccine`);
   return response.data;
 }
 
 export async function apiAddVaccines(id: string, body: { name: string, inventory: number }) {
-  axios.post(`${API_ENDPOINT}/v1/clinic/${id}/vaccine`, body);
+  axios.post(`/v1/clinic/${id}/vaccine`, body);
 }
 
 export async function authenticate() {
-  axios.get(`${API_ENDPOINT}/auth/google`);
+  axios.get(`/auth/google`);
 }
 
--- src/api/types/Patient.ts
+++ src/api/types/Patient.ts
@@ -1,6 +1,6 @@
 export interface Patient {
-  patientId: string;
-  userId: string;
-  priority: string;
+  patient_id: string;
+  user_id: string;
+  priority: number;
   postalCode: string;
 }
