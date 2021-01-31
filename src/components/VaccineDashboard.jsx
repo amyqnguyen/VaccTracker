8e56f971a21c2fcae40bba336a810184a423c32f
Jeffrey Kwok <47469240+jkwok2@users.noreply.github.com>
2021-01-31 03:46:17 -0800

    added dialog for dashboard

--- src/components/VaccineDashboard.jsx
+++ src/components/VaccineDashboard.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Title from './Title';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 function preventDefault(event) {
   event.preventDefault();
@@ -18,9 +24,57 @@ const useStyles = makeStyles({
   },
 });
 
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+
 export default function Deposits() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <React.Fragment>
           <Card className={classes.root}>
@@ -33,9 +87,19 @@ export default function Deposits() {
         on 15 January, 2021
       </Typography>
       <div>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          View total vaccines available
+        <Link color="primary" href="#" onClick={handleClickOpen}>
+          View detailed vaccine statistics
         </Link>
+        <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Vaccine Details
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Insert Vaccine details here from database
+          </Typography>
+        </DialogContent>
+      </Dialog>
       </div>
       </CardContent>
     </Card>
